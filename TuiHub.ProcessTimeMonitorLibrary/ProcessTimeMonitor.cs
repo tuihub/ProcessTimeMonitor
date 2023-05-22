095e680e2200fd300a860d6d933c2aa10d216440
Tabing010102 <tabing010102@outlook.com>
2023-05-22 11:15:02 +0800

    add TuiHub.ProcessTimeMonitorLibrary

---
+++ TuiHub.ProcessTimeMonitorLibrary/ProcessTimeMonitor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TuiHub.ProcessTimeMonitorLibrary
+{
+    public partial class ProcessTimeMonitor<T>
+    {
+        private readonly ILogger<T>? _logger;
+        public ProcessTimeMonitor(ILogger<T>? logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ TuiHub.ProcessTimeMonitorLibrary/Services/WaitForProcToExit.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuiHub.ProcessTimeMonitorLibrary
+{
+    public partial class ProcessTimeMonitor<T>
+    {
+        public async Task WaitForProcToExit(string name, string? path = null, int sleepCountLimit = 10, int sleepMilliseconds = 1000)
+        {
+            _logger?.LogDebug("WaitForProcToExit", $"name = {name}, path = {(path == null ? "null" : path)}");
+            Process[] processes = Process.GetProcessesByName(name);
+            int sleepCount = 0;
+            while (processes.Length == 0)
+            {
+                if (sleepCount > sleepCountLimit)
+                {
+                    _logger?.LogWarning("WaitForProcToExit", $"Sleep time limit exceeded, exiting.");
+                    throw new TimeoutException($"Sleep time(sleepCountLimit = {sleepCountLimit}, sleepMilliseconds = {sleepMilliseconds}) limit exceeded.");
+                }
+                _logger?.LogDebug("WaitForProcToExit", $"processes.Length == 0, sleeping for {sleepMilliseconds}ms({sleepCount} / {sleepCountLimit}).");
+                sleepCount++;
+                Thread.Sleep(sleepMilliseconds);
+                processes = Process.GetProcessesByName(name);
+                if (path != null)
+                    processes = processes.Where(x => x.MainModule != null && x.MainModule.FileName == path).ToArray();
+            }
+            foreach (var process in processes)
+                _logger?.LogDebug("WaitForProcToExit/GetProcessesByName", $"process [id = {process.Id}, name = {process.ProcessName}, path = {(process.MainModule == null ? "null" : process.MainModule.FileName)}]");
+            _logger?.LogDebug("WaitForProcToExit", $"path = {(path == null ? "null" : path)}");
+            if (path != null)
+            {
+                processes = processes.Where(x => x.MainModule != null && x.MainModule.FileName == path).ToArray();
+                foreach (var process in processes)
+                    _logger?.LogDebug("WaitForProcToExit/GetProcessesByNameFiltered", $"process [id = {process.Id}, name = {process.ProcessName}, path = {(process.MainModule == null ? "null" : process.MainModule.FileName)}]");
+            }
+            foreach (var process in processes)
+            {
+                _logger?.LogDebug("WaitForProcToExit", $"Wait for process[id = {process.Id}, name = {process.ProcessName}, path = {(process.MainModule == null ? "null" : process.MainModule.FileName)}] to exit.");
+                await process.WaitForExitAsync();
+            }
+        }
+    }
+}
